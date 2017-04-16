6d5f619f223455c790e43d8ddc1433a7390d1fd1
embrown <ebrown37@mail.csuchico.edu>
2017-04-16 08:54:59 -0700

    Fleshed out the Status component

--- app/static/scripts/jsx/CharacterSheet.js
+++ app/static/scripts/jsx/CharacterSheet.js
@@ -1,4 +1,4 @@
-var $=require('jquery');
+var $=require('../../bower_components/jquery/dist/jquery');
 import Abilities from './sheet_components/Abilities'
 import Description from './sheet_components/Description'
 import Levels from './sheet_components/Levels'
